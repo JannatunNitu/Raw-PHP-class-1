65bf81a809dc654f9e754fa755de00d89b152b42
Jannatun Nayeem Nitu <110406009+JannatunNitu@users.noreply.github.com>
2023-08-25 23:29:12 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/** * PHP full form Hyper Text Prepocessor
+ * PHP is server side scripting language
+ * PHP can generate dynamic page content
+ */
+
+//  PHP echo statement
+echo "<h1>Hello World</h1>";
+echo "<h2>Hello World</h2>";
+
+// use of variable
+$name = "My name is Jannatun Nayeem Nitu <br><br>";
+$skill = "Frontend developer <br><br>";
+
+echo $name;
+echo $skill;
+
+$college = "Chittagong Mohila Polytechnic Institute";
+echo 'Institute name :' .$college ."<br><br>";
+
+// Concatenation in PHP
+$age = 20;
+
+echo 'I am ' . $age . ' years old. <br> <br>';
+
+// contants
+define ("year", "I have 1 years of experience in frontend development. ");
+echo year;
+define ("lang", " I used programming languages such as HTML, CSS, JavaScript, Bootstrap, SASS, and jQuery to create websites that are user-friendly and responsive.");
+echo lang;
+
+
